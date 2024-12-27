ed53e92a204fb182180daf775b3ffe8a737a4176
Sugeesan <54391463+Sugeesan@users.noreply.github.com>
2024-12-28 00:08:43 +1100

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_library",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies here
+    description="A sample Python library",
+    author="focusdatai",
+    author_email="your.email@example.com",
+    url="https://github.com/focusdatai/my_library",  # Update with your repo URL
+)
