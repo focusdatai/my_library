d54c6490af4b830f0dfea5bd1d09a14b80aa8d80
Sugeesan <54391463+Sugeesan@users.noreply.github.com>
2024-12-28 09:33:34 +1100

    Added delta table functions

--- my_library/module1.py
+++ my_library/module1.py
@@ -9,3 +9,13 @@ def CreateDeltaTableTst(dataFrame, targetTableFqn, dataLakePath):
     .mode("overwrite") \
     .save(dataLakePath)
   spark.sql(f"CREATE TABLE IF NOT EXISTS {targetTableFqn} USING DELTA LOCATION \'{dataLakePath}\'")
+  
+def BasicMerge(sourceDataFrame, targetTableFqn, businessKey=None):
+  businessKey = spark.table(targetTableFqn).columns[0] if businessKey is None else businessKey
+  s = ConcatBusinessKey(businessKey, "s")
+  t = ConcatBusinessKey(businessKey, "t")
+  
+  df = DeltaTable.forName(spark, targetTableFqn).alias("t").merge(sourceDataFrame.alias("s"), f"{s} = {t}") \
+    .whenMatchedUpdateAll() \
+    .whenNotMatchedInsertAll() \
+    .execute()  
