47da055cdce4f8794be22a12310e08e2c362aa2a
Sugeesan <54391463+Sugeesan@users.noreply.github.com>
2024-12-28 08:37:37 +1100

    Added delta table functions

--- my_library/module1.py
+++ my_library/module1.py
@@ -1,5 +1,14 @@
 from delta.tables import *
 
+def CreateDeltaTableTst(dataFrame, targetTableFqn, dataLakePath):
+  dataFrame.write \
+    .format("delta") \
+    .option("mergeSchema", "true") \
+    .mode("overwrite") \
+    .save(dataLakePath)
+  spark.sql(f"CREATE TABLE IF NOT EXISTS {targetTableFqn} USING DELTA LOCATION \'{dataLakePath}\'")
+
+
 def CreateDeltaTable(dataFrame, targetTableFqn, dataLakePath):
   dataFrame.write \
     .format("delta") \
