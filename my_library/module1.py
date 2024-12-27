b01030234210149d267e6352eedcf66fad6168d2
Sugeesan <54391463+Sugeesan@users.noreply.github.com>
2024-12-28 08:27:07 +1100

    Added delta table functions

--- my_library/module1.py
+++ my_library/module1.py
@@ -1,2 +1,25 @@
-def greet(name):
-    return f"Hello, {name}!"
+from delta.tables import *
+
+def CreateDeltaTable(dataFrame, targetTableFqn, dataLakePath):
+  dataFrame.write \
+    .format("delta") \
+    .option("mergeSchema", "true") \
+    .mode("overwrite") \
+    .save(dataLakePath)
+  spark.sql(f"CREATE TABLE IF NOT EXISTS {targetTableFqn} USING DELTA LOCATION \'{dataLakePath}\'")
+
+  def BasicMerge(sourceDataFrame, targetTableFqn, businessKey=None):
+  businessKey = spark.table(targetTableFqn).columns[0] if businessKey is None else businessKey
+  s = ConcatBusinessKey(businessKey, "s")
+  t = ConcatBusinessKey(businessKey, "t")
+  
+  df = DeltaTable.forName(spark, targetTableFqn).alias("t").merge(sourceDataFrame.alias("s"), f"{s} = {t}") \
+    .whenMatchedUpdateAll() \
+    .whenNotMatchedInsertAll() \
+    .execute()
+  
+  def CreateOrMerge(sourceDataFrame, targetTableFqn, dataLakePath, businessKey=None):
+  if (TableExists(targetTableFqn)):
+    BasicMerge(sourceDataFrame, targetTableFqn, businessKey)
+  else:
+    CreateDeltaTable(sourceDataFrame, targetTableFqn, dataLakePath)  
